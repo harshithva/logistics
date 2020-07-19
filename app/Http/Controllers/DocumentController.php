4fb889b9d1acea8452d89e6373ed7403ccef7a09
harshithva <harshith11032001@gmail.com>
2020-07-19 21:44:02 +0530

    Document model and migration created

---
+++ app/Http/Controllers/DocumentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Document;
+use Illuminate\Http\Request;
+
+class DocumentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Document $document)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Document $document)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Document $document)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Document $document)
+    {
+        //
+    }
+}
