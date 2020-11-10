99985cac484520e7a5d07f84204fecb257dbf2a0
harshithva <harshith11032001@gmail.com>
2020-11-10 22:31:42 +0530

    terms added in quote

--- resources/js/components/customer-components/CustomerDashboardQuoteView.vue
+++ resources/js/components/customer-components/CustomerDashboardQuoteView.vue
@@ -4,7 +4,10 @@
       <div class="col"></div>
       <div class="col"></div>
       <div class="col">
-        <a class="btn btn-primary text-white" onclick="javascript:window.print()">
+        <a
+          class="btn btn-primary text-white"
+          onclick="javascript:window.print()"
+        >
           <i class="fas fa-print"></i> Print
         </a>
 
@@ -32,16 +35,15 @@
       <div class="card-body">
         <div class="row mt-2">
           <div class="col">
-            <img :src="logo" style="max-width:8rem" />
+            <img :src="logo" style="max-width: 8rem" />
           </div>
           <div class="col"></div>
           <div class="col">
             <p>
               GURUKAL LOGISTICS
-              <br />Anchepalya, Bangalore - 560073
-              <br />Mob: +91 9620202001
-              <br />E-mail : logistics@gurukal.co.in
-              <br />Website: www.gurukal.co.in
+              <br />Anchepalya, Bangalore - 560073 <br />Mob: +91 9620202001
+              <br />E-mail : logistics@gurukal.co.in <br />Website:
+              www.gurukal.co.in
               <br />
             </p>
           </div>
@@ -49,10 +51,12 @@
         <hr />
         <div class="row">
           <div class="col">
-            <p>Ref No: {{quote.quotation_no}}</p>
+            <p>Ref No: {{ quote.quotation_no }}</p>
           </div>
           <div class="col"></div>
-          <div class="col">Date: {{ moment(quote.created_at).format('DD/MM/YYYY')}}</div>
+          <div class="col">
+            Date: {{ moment(quote.created_at).format("DD/MM/YYYY") }}
+          </div>
         </div>
 
         <div class="row mt-2">
@@ -68,11 +72,11 @@
             <p>
               To,
               <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{quote.customer.company_name}}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ quote.customer.company_name }}
               <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{quote.customer.address}}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ quote.customer.address }}
               <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{quote.customer.gst}}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ quote.customer.gst }}
               <br />
             </p>
           </div>
@@ -80,7 +84,13 @@
 
         <div class="row mt-2">
           <div class="col text-center">
-            <h6>Sub: Transit Quote for {{quote.list[0].to.charAt(0).toUpperCase() + quote.list[0].to.slice(1)}}</h6>
+            <h6>
+              Sub: Transit Quote for
+              {{
+                quote.list[0].to.charAt(0).toUpperCase() +
+                quote.list[0].to.slice(1)
+              }}
+            </h6>
           </div>
         </div>
 
@@ -89,7 +99,13 @@
             <p>
               Dear Sir,
               <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;As per our discussion regarding transportation requirement for the consignment to {{quote.list[0].to.charAt(0).toUpperCase() + quote.list[0].to.slice(1)}}.
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;As
+              per our discussion regarding transportation requirement for the
+              consignment to
+              {{
+                quote.list[0].to.charAt(0).toUpperCase() +
+                quote.list[0].to.slice(1)
+              }}.
             </p>
           </div>
         </div>
@@ -115,15 +131,15 @@
                 <th scope="col">Rate</th>
                 <th scope="col">Advance</th>
               </thead>
-              <tr v-if="quote.list" v-for="(item,index) in quote.list">
-                <th scope="row">{{index+1}}</th>
-                <td>{{item.from}}</td>
-                <td>{{item.to}}</td>
-                <td>{{item.description}}</td>
-                <td>{{item.size}} & {{item.weight}}</td>
-                <td>{{item.eta}} days</td>
-                <td>{{item.rate}}</td>
-                <td>{{item.advance}}</td>
+              <tr v-if="quote.list" v-for="(item, index) in quote.list">
+                <th scope="row">{{ index + 1 }}</th>
+                <td>{{ item.from }}</td>
+                <td>{{ item.to }}</td>
+                <td>{{ item.description }}</td>
+                <td>{{ item.size }} & {{ item.weight }}</td>
+                <td>{{ item.eta }} days</td>
+                <td>{{ item.rate }}</td>
+                <td>{{ item.advance }}</td>
               </tr>
             </table>
           </div>
@@ -133,10 +149,25 @@
             <h6>Terms & Conditions</h6>
             <ol>
               <li>Rates quoted above are valid for 48Hrs only.</li>
-              <li>Extra charges will be implied If the consignment exceeds the limit of fleet dimension.</li>
-              <li>Advance payment is to be made (unless stated above, in which case you shall pay the quoted price).</li>
-              <li>if the unloading time exceeds 4Hrs halting will be charged on pro rate basis.</li>
-              <li>Payments are to be made within 7 days from the date of invoice.</li>
+              <li>
+                Extra charges will be implied If the consignment exceeds the
+                limit of fleet dimension/ RTO Regulations.
+              </li>
+              <li>
+                Advance payment is to be made (unless stated above, in which
+                case you shall pay the quoted price).
+              </li>
+              <li>
+                If the unloading time exceeds 4hrs halting will be charged as
+                per actuals
+              </li>
+              <li>
+                Payments are to be made within 7 days from the date of invoice.
+              </li>
+              <li>
+                If Vehicle Is Cancelled Prior To 60min/On Arrival Charges Are
+                Applicable.
+              </li>
             </ol>
           </div>
         </div>
@@ -145,10 +176,8 @@
           <div class="col">
             <p>
               Bank Details
-              <br />Name : Axis Bank
-              <br />8th Mile Branch
-              <br />A/c No.: 918020030455515
-              <br />IFSC: UTIB0002926
+              <br />Name : Axis Bank <br />8th Mile Branch <br />A/c No.:
+              918020030455515 <br />IFSC: UTIB0002926
               <br />
             </p>
           </div>
@@ -158,7 +187,12 @@
           <div class="col-10"></div>
           <div class="col">
             <p>For and behalf of</p>
-            <img :src="sign" alt="Rohith" class="img-fluid ml-3" style="width:6rem;" />
+            <img
+              :src="sign"
+              alt="Rohith"
+              class="img-fluid ml-3"
+              style="width: 6rem"
+            />
             <br />
             <br />
 
@@ -176,7 +210,7 @@ export default {
     return {
       showUpdation: true,
       logo: "https://i.ibb.co/WFdrW4M/Logo-Color-Text-Below.jpg",
-      sign: "https://i.ibb.co/W6vkYqs/seal.png"
+      sign: "https://i.ibb.co/W6vkYqs/seal.png",
     };
   },
   methods: {
@@ -184,39 +218,39 @@ export default {
       if (status == "approve") {
         axios
           .post(`/api/quotations/${this.quote.id}/status/approve`)
-          .then(response => {
+          .then((response) => {
             this.$store.dispatch(
               "retrieveSingleQuote",
               this.$route.params.quote_id
             );
             this.showUpdation = false;
           })
-          .catch(function(error) {
+          .catch(function (error) {
             console.log("Something went wrong");
           });
       } else if (status == "decline") {
         axios
           .post(`/api/quotations/${this.quote.id}/status/decline`)
-          .then(response => {
+          .then((response) => {
             this.$store.dispatch(
               "retrieveSingleQuote",
               this.$route.params.quote_id
             );
             this.showUpdation = false;
           })
-          .catch(function(error) {
+          .catch(function (error) {
             console.log("Something went wrong");
           });
       }
-    }
+    },
   },
   computed: {
     quote() {
       return new Form(this.$store.getters.getSingleQuote);
-    }
+    },
   },
   created() {
     this.$store.dispatch("retrieveSingleQuote", this.$route.params.quote_id);
-  }
+  },
 };
 </script>
--- resources/js/components/customers/CustomerQuoteView.vue
+++ resources/js/components/customers/CustomerQuoteView.vue
@@ -3,7 +3,10 @@
     <div class="row mt-3 mb-3 ml-3 d-print-none">
       <div class="col-6"></div>
       <div class="col-6">
-        <a class="btn btn-primary text-white" onclick="javascript:window.print()">
+        <a
+          class="btn btn-primary text-white"
+          onclick="javascript:window.print()"
+        >
           <i class="fas fa-print"></i> Print
         </a>
 
@@ -26,7 +29,7 @@
           <i class="fas fa-check"></i> Approve
         </button>
         <router-link
-          :to="'/admin/quotes/'+quote.id +'/edit'"
+          :to="'/admin/quotes/' + quote.id + '/edit'"
           class="btn btn-warning ml-2"
           v-if="this.$store.getters.getUserData.user.role == 'admin'"
         >
@@ -38,16 +41,15 @@
       <div class="card-body">
         <div class="row mt-2">
           <div class="col">
-            <img :src="logo" style="max-width:8rem" />
+            <img :src="logo" style="max-width: 8rem" />
           </div>
           <div class="col"></div>
           <div class="col">
             <p>
               GURUKAL LOGISTICS
-              <br />Anchepalya, Bangalore - 560073
-              <br />Mob: +91 9620202001
-              <br />E-mail : logistics@gurukal.co.in
-              <br />Website: www.gurukal.co.in
+              <br />Anchepalya, Bangalore - 560073 <br />Mob: +91 9620202001
+              <br />E-mail : logistics@gurukal.co.in <br />Website:
+              www.gurukal.co.in
               <br />
             </p>
           </div>
@@ -55,10 +57,12 @@
         <hr />
         <div class="row">
           <div class="col">
-            <p>Ref No: {{quote.quotation_no}}</p>
+            <p>Ref No: {{ quote.quotation_no }}</p>
           </div>
           <div class="col"></div>
-          <div class="col">Date: {{ moment(quote.created_at).format('DD/MM/YYYY')}}</div>
+          <div class="col">
+            Date: {{ moment(quote.created_at).format("DD/MM/YYYY") }}
+          </div>
         </div>
 
         <div class="row mt-2">
@@ -74,11 +78,11 @@
             <p>
               To,
               <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{quote.customer.company_name}}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ quote.customer.company_name }}
               <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{quote.customer.address}}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ quote.customer.address }}
               <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{quote.customer.gst}}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ quote.customer.gst }}
               <br />
             </p>
           </div>
@@ -86,7 +90,13 @@
 
         <div class="row mt-2">
           <div class="col text-center">
-            <h6>Sub: Transit Quote for {{quote.list[0].to.charAt(0).toUpperCase() + quote.list[0].to.slice(1)}}</h6>
+            <h6>
+              Sub: Transit Quote for
+              {{
+                quote.list[0].to.charAt(0).toUpperCase() +
+                quote.list[0].to.slice(1)
+              }}
+            </h6>
           </div>
         </div>
 
@@ -95,7 +105,13 @@
             <p>
               Dear Sir,
               <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;As per our discussion regarding transportation requirement for the consignment to {{quote.list[0].to.charAt(0).toUpperCase() + quote.list[0].to.slice(1)}}.
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;As
+              per our discussion regarding transportation requirement for the
+              consignment to
+              {{
+                quote.list[0].to.charAt(0).toUpperCase() +
+                quote.list[0].to.slice(1)
+              }}.
             </p>
           </div>
         </div>
@@ -121,15 +137,15 @@
                 <th scope="col">Rate</th>
                 <th scope="col">Advance</th>
               </thead>
-              <tr v-if="quote.list" v-for="(item,index) in quote.list">
-                <th scope="row">{{index+1}}</th>
-                <td>{{item.from}}</td>
-                <td>{{item.to}}</td>
-                <td>{{item.description}}</td>
-                <td>{{item.size}} & {{item.weight}}</td>
-                <td>{{item.eta}} days</td>
-                <td>{{item.rate}}</td>
-                <td>{{item.advance}}</td>
+              <tr v-if="quote.list" v-for="(item, index) in quote.list">
+                <th scope="row">{{ index + 1 }}</th>
+                <td>{{ item.from }}</td>
+                <td>{{ item.to }}</td>
+                <td>{{ item.description }}</td>
+                <td>{{ item.size }} & {{ item.weight }}</td>
+                <td>{{ item.eta }} days</td>
+                <td>{{ item.rate }}</td>
+                <td>{{ item.advance }}</td>
               </tr>
             </table>
           </div>
@@ -139,10 +155,25 @@
             <h6>Terms & Conditions</h6>
             <ol>
               <li>Rates quoted above are valid for 48Hrs only.</li>
-              <li>Extra charges will be implied If the consignment exceeds the limit of fleet dimension.</li>
-              <li>Advance payment is to be made (unless stated above, in which case you shall pay the quoted price).</li>
-              <li>if the unloading time exceeds 4Hrs halting will be charged on pro rate basis.</li>
-              <li>Payments are to be made within 7 days from the date of invoice.</li>
+              <li>
+                Extra charges will be implied If the consignment exceeds the
+                limit of fleet dimension/ RTO Regulations.
+              </li>
+              <li>
+                Advance payment is to be made (unless stated above, in which
+                case you shall pay the quoted price).
+              </li>
+              <li>
+                If the unloading time exceeds 4hrs halting will be charged as
+                per actuals
+              </li>
+              <li>
+                Payments are to be made within 7 days from the date of invoice.
+              </li>
+              <li>
+                If Vehicle Is Cancelled Prior To 60min/On Arrival Charges Are
+                Applicable.
+              </li>
             </ol>
           </div>
         </div>
@@ -151,10 +182,8 @@
           <div class="col">
             <p>
               Bank Details
-              <br />Name : Axis Bank
-              <br />8th Mile Branch
-              <br />A/c No.: 918020030455515
-              <br />IFSC: UTIB0002926
+              <br />Name : Axis Bank <br />8th Mile Branch <br />A/c No.:
+              918020030455515 <br />IFSC: UTIB0002926
               <br />
             </p>
           </div>
@@ -164,7 +193,12 @@
           <div class="col-10"></div>
           <div class="col">
             <p>For and behalf of</p>
-            <img :src="sign" alt="Rohith" class="img-fluid ml-3" style="width:6rem;" />
+            <img
+              :src="sign"
+              alt="Rohith"
+              class="img-fluid ml-3"
+              style="width: 6rem"
+            />
             <br />
             <br />
 
@@ -182,7 +216,7 @@ export default {
     return {
       showUpdation: true,
       logo: "https://i.ibb.co/WFdrW4M/Logo-Color-Text-Below.jpg",
-      sign: "https://i.ibb.co/W6vkYqs/seal.png"
+      sign: "https://i.ibb.co/W6vkYqs/seal.png",
     };
   },
   methods: {
@@ -190,27 +224,27 @@ export default {
       if (status == "approve") {
         axios
           .post(`/api/quotations/${this.quote.id}/status/approve`)
-          .then(response => {
+          .then((response) => {
             this.$store.dispatch(
               "retrieveSingleQuote",
               this.$route.params.quote_id
             );
             this.showUpdation = false;
           })
-          .catch(function(error) {
+          .catch(function (error) {
             console.log("Something went wrong");
           });
       } else if (status == "decline") {
         axios
           .post(`/api/quotations/${this.quote.id}/status/decline`)
-          .then(response => {
+          .then((response) => {
             this.$store.dispatch(
               "retrieveSingleQuote",
               this.$route.params.quote_id
             );
             this.showUpdation = false;
           })
-          .catch(function(error) {
+          .catch(function (error) {
             console.log("Something went wrong");
           });
       }
@@ -218,32 +252,32 @@ export default {
     sendQuoteLink() {
       axios
         .post(`api/quotations/${this.quote.id}/quote_send_email`)
-        .then(response => {
+        .then((response) => {
           Swal.fire({
             position: "top-end",
             icon: "success",
             title: "Quote has sent through mail",
             showConfirmButton: false,
-            timer: 1500
+            timer: 1500,
           });
         })
-        .catch(err => {
+        .catch((err) => {
           Swal.fire({
             icon: "error",
             title: "Oops...",
             text: "Something went wrong!",
-            footer: "Make sure customer email is valid."
+            footer: "Make sure customer email is valid.",
           });
         });
-    }
+    },
   },
   computed: {
     quote() {
       return new Form(this.$store.getters.getSingleQuote);
-    }
+    },
   },
   created() {
     this.$store.dispatch("retrieveSingleQuote", this.$route.params.quote_id);
-  }
+  },
 };
 </script>
